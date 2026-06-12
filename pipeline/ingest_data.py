46e8e224a286bacfdddf8e6024ee3a80c629846a
Kothapalli Venkata Subba Rami Reddy <60468027+19PA1A0484@users.noreply.github.com>
2026-06-12 07:44:07 +0000

    my-new-docker-code

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -33,38 +34,48 @@ parse_dates = [
 
 
 
-def run():
-    pg_user = 'root'
-    pg_pass='root'
-    pg_host='localhost'
-    pg_port = 5432
-    pg_db= 'ny_taxi'
-
-    year = 2021
-    month = 1
-    chunksize=100000
-
-    target_table = 'yellow_taxi_data'
+@click.command()
+@click.option('--pg-user', default='root', show_default=True, help='Postgres user name')
+@click.option('--pg-pass', default='root', show_default=True, help='Postgres password')
+@click.option('--pg-host', default='localhost', show_default=True, help='Postgres host')
+@click.option('--pg-port', default=5432, show_default=True, type=int, help='Postgres port')
+@click.option('--pg-db', default='ny_taxi', show_default=True, help='Postgres database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--year', default=2021, show_default=True, type=int, help='Year for the taxi dataset')
+@click.option('--month', default=1, show_default=True, type=int, help='Month for the taxi dataset')
+@click.option('--chunksize', default=100000, show_default=True, type=int, help='Number of rows to read per chunk')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, year, month, chunksize):
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
 
-    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    # build URL without introducing a double-slash
+    url = f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+    try:
+        engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+    except Exception as exc:
+        raise click.ClickException(f'Failed to create DB engine: {exc}')
 
-    df_iter = pd.read_csv(url,dtype=dtype,parse_dates=parse_dates,iterator=True,chunksize=chunksize)
+    try:
+        df_iter = pd.read_csv(url, dtype=dtype, parse_dates=parse_dates, iterator=True, chunksize=chunksize)
+    except Exception as exc:
+        raise click.ClickException(f'Failed to read CSV from {url}: {exc}')
 
     first = True
     for df_chunk in tqdm(df_iter):
         if first:
-            df_chunk.head(0).to_sql(name=target_table, con=engine,if_exists='replace')
+            df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists='replace')
             first = False
 
-        df_chunk.to_sql(name=target_table, con=engine,if_exists='append')
+        df_chunk.to_sql(name=target_table, con=engine, if_exists='append')
 
 
 if __name__ == '__main__':
     run()
+
+
+
+
 
 
 
